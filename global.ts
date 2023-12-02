7b97389eeecb0982955256f8da96f5ec3b899245
Milan Jelínek <mjelinek@artex-is.cz>
2023-12-03 00:00:56 +0100

    Boilerplate

--- global.ts
+++ global.ts
@@ -1,4 +1,15 @@
 import fs from 'fs';
+import { colors } from './types';
+import log from './log';
+
+export function logResult(title: string, result: number, expected?: number) {
+    const pass = result === expected;
+
+    if (expected !== undefined)
+        log((pass ? colors.fg.green : colors.fg.red) + `${title}: ${result} ${pass ? '===' : '!=='} ${expected} ${pass ? "PASS" : "FAIL"}`);
+    else
+        log(colors.fg.yellow + `${title}: ${result}`);
+}
 
 export function loadLines(path: string) {
     return fs.readFileSync(path).toString().split('\r\n');
