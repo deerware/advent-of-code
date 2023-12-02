d9d71825fe1a5402d138b87d174eb2ebc8216e52
Milan Jelínek <mjelinek@artex-is.cz>
2023-12-02 09:33:52 +0100

    Day 2: Cube Conundrum part 1

--- index.ts
+++ index.ts
@@ -1,7 +1,12 @@
 import trebuchet from './01_Trebuchet/trebuchet';
+import cube_conundrum from './02_Cube_Conundrum/cube_conundrum';
 import log from './log';
 
-trebuchet();
+try {
+    cube_conundrum();
+} catch (e) {
+    log('Error:', (e as any).message);
+}
 
 log('Waiting 60s to exit...');
 setTimeout(() => { log('Exit') }, 60000);
