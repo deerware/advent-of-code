717101d3f080329c9bfc388074e215d47f98e710
Milan Jelínek <mjelinek@artex-is.cz>
2023-12-03 09:02:47 +0100

    Updated boilerplate for tomorrow

--- 00_Boilerplate/boilerplate.ts
+++ 00_Boilerplate/boilerplate.ts
@@ -1,34 +1,26 @@
-import fs from 'fs';
 import log from '../log'
 import { colors } from '../types'
-import { loadLines } from '../global';
+import { loadLines, logResult } from '../global';
 
 export default async function main() {
     const startTime = new Date();
     log("Day XX");
 
-    const p1test = await part1(loadLines('<<SAMPLE1>>'));
-    const p1testr = 0;
-    log((p1test === p1testr ? colors.fg.green : colors.fg.red) + `Part 1 test: ${p1test} ${p1test === p1testr ? '===' : '!=='} ${p1testr} ${p1test === p1testr ? "PASS" : "FAIL"}`);
-
-    const p1 = part1(loadLines('<<INPUT>>'));
-    log(colors.fg.yellow + `Part 1: ${p1}`);
+    logResult('Part 1 test', await part1(loadLines('<<DAY>>/sampleData1.txt')), 4361)
+    logResult('Part 1', await part1(loadLines('<<DAY>>/input.txt')))
 
     log();
 
-    const p2test = await part2(loadLines('<<SAMPLE2>>'));
-    const p2testr = 0;
-    log((p2test === p2testr ? colors.fg.green : colors.fg.red) + `Part 2 test: ${p2test} ${p2test === p2testr ? '===' : '!=='} ${p2testr} ${p2test === p2testr ? "PASS" : "FAIL"}`);
+    logResult('Part 2 test', await part2(loadLines('<<DAY>>/sampleData1.txt')), 467835)
+    logResult('Part 2', await part2(loadLines('<<DAY>>/input.txt')))
 
-    const p2 = part2(loadLines('<<INPUT>>'));
-    log(colors.fg.yellow + `Part 2: ${p2}`);
     log(colors.fg.gray + `Executed in ${(new Date().getTime() - startTime.getTime())}ms`);
 }
 
 async function part1(data: string[]): Promise<number> {
-    throw new Error('Not implemented');
+    return -Infinity;
 }
 
 async function part2(data: string[]): Promise<number> {
-    throw new Error('Not implemented');
+    return -Infinity;
 }
