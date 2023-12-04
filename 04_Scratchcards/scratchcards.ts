a3177410965e2e228586bc846099b4bd3180f194
Milan Jelínek <mjelinek@artex-is.cz>
2023-12-04 06:09:52 +0100

    Day 4: Scratchcards boilerplate

---
+++ 04_Scratchcards/scratchcards.ts
@@ -0,0 +1,26 @@
+import log from '../log'
+import { colors } from '../types'
+import { loadLines, logResult } from '../global';
+
+export default async function main() {
+    const startTime = new Date();
+    log("Day 4: Scratchcards");
+
+    if (logResult('Part 1 test', await part1(loadLines('04_Scratchcards/sampleData1.txt')), 0))
+        logResult('Part 1', await part1(loadLines('04_Scratchcards/input.txt')))
+
+    log();
+
+    if (logResult('Part 2 test', await part2(loadLines('04_Scratchcards/sampleData2.txt')), 0))
+        logResult('Part 2', await part2(loadLines('04_Scratchcards/input.txt')))
+
+    log(colors.fg.gray + `Executed in ${(new Date().getTime() - startTime.getTime())}ms`);
+}
+
+async function part1(data: string[]): Promise<number> {
+    return -Infinity;
+}
+
+async function part2(data: string[]): Promise<number> {
+    return -Infinity;
+}
--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
 import trebuchet from './01_Trebuchet/trebuchet';
 import cube_conundrum from './02_Cube_Conundrum/cube_conundrum';
 import gear_ratios from './03_Gear_Ratios/gear_ratios'
+import scratchcards from './04_Scratchcards/scratchcards';
 import log from './log';
 
 try {
     // trebuchet();
     // cube_conundrum();
-    gear_ratios();
+    // gear_ratios();
+    scratchcards();
 } catch (e) {
     log('Error:', (e as any).message);
 }
