0d4e091b4bad821e55470c7fc34015bbfaaa19af
Milan Jelínek <mjelinek@artex-is.cz>
2023-12-05 09:15:20 +0100

    Day 5: If You Give A Seed A Fertilizer boilerplate

--- index.ts
+++ index.ts
@@ -2,14 +2,18 @@ import trebuchet from './01_Trebuchet/trebuchet';
 import cube_conundrum from './02_Cube_Conundrum/cube_conundrum';
 import gear_ratios from './03_Gear_Ratios/gear_ratios'
 import scratchcards from './04_Scratchcards/scratchcards';
+import seedfertilizer from './05_Seed_Fertilizer/seedfertilizer';
 import log from './log';
 
-try {
-    // trebuchet();
-    // cube_conundrum();
-    // gear_ratios();
-    scratchcards();
-} catch (e) {
-    log('Error:', (e as any).message);
-}
-setTimeout(() => { log('Exit') }, 60 * 60 * 1000);
+(async () => {
+    try {
+        // await trebuchet();
+        // await cube_conundrum();
+        // await gear_ratios();
+        // await scratchcards();
+        await seedfertilizer();
+    } catch (e) {
+        log('Error:', (e as any).message);
+    }
+    setTimeout(() => { log('Exit') }, 60 * 60 * 1000);
+})();
--- log.ts
+++ log.ts
@@ -1,10 +1,12 @@
+import { colors } from "./types";
+
 const days = ["neděle", "pondělí", "úterý", "středa", "čtvrtek", "pátek", "sobota"];
 let day: number;
 
 export default function log(...params: any[]) {
     let date = new Date();
     checkDay(date);
-    console.log("[" + (new Date().toLocaleTimeString('cs-CZ', { hour: "2-digit", minute: "2-digit", second: "2-digit" })) + "]", ...params);
+    console.log(colors.reset + "[" + (new Date().toLocaleTimeString('cs-CZ', { hour: "2-digit", minute: "2-digit", second: "2-digit" })) + "]", ...params);
 }
 
 function checkDay(date: Date) {
