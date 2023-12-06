6b7706533f72c1796675bf70aeaa98ab05e52272
Milan Jelínek <mjelinek@artex-is.cz>
2023-12-06 21:14:17 +0100

    Day 6: Wait For It part 1

--- 06_Wait_For_It/waitforit.ts
+++ 06_Wait_For_It/waitforit.ts
@@ -11,14 +11,39 @@ export default async function main() {
 
     log();
 
-    if (logResult('Part 2 test', await part2(loadLines('06_Wait_For_It/sampleData2.txt')), 0))
+    if (logResult('Part 2 test', await part2(loadLines('06_Wait_For_It/sampleData1.txt')), 0))
         logResult('Part 2', await part2(loadLines('06_Wait_For_It/input.txt')))
 
     log(colors.fg.gray + `Executed in ${(new Date().getTime() - startTime.getTime())}ms`);
 }
 
 async function part1(data: string[]): Promise<number> {
-    return -Infinity;
+    const races: { time: number, target: number }[] = [];
+
+    const times = data[0].split(/ +/).slice(1).map(x => parseInt(x));
+    const targets = data[1].split(/ +/).slice(1).map(x => parseInt(x));
+    if (times.length !== targets.length || times.length === 0)
+        throw new Error('Parse error or invalid input');
+
+    for (const time in times) {
+        races.push({
+            time: times[time],
+            target: targets[time],
+        });
+    }
+
+    let sum = 1;
+
+    for (const race of races) {
+        let win = 0;
+        for (let i = 1; i <= race.time; i++) {
+            if (i * (race.time - i) > race.target)
+                win++;
+        }
+        sum *= win;
+    }
+
+    return sum;
 }
 
 async function part2(data: string[]): Promise<number> {
