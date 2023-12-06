b17d33c2c35e2a20a867fb2eb3d994702e24832e
Milan Jelínek <mjelinek@artex-is.cz>
2023-12-06 20:55:52 +0100

    Day 5: If You Give A Seed A Fertilizer part 2 - unsuccessful draft

--- 05_Seed_Fertilizer/seedfertilizer.ts
+++ 05_Seed_Fertilizer/seedfertilizer.ts
@@ -1,24 +1,96 @@
 import log from '../log'
 import { colors } from '../types'
+import * as global from '../global';
 import { loadLines, logResult } from '../global';
 
 export default async function main() {
-    const startTime = new Date();
     log("Day 5: If You Give A Seed A Fertilizer");
 
-    if (logResult('Part 1 test', await part1(loadLines('05_Seed_Fertilizer/sampleData1.txt')), 35))
-        logResult('Part 1', await part1(loadLines('05_Seed_Fertilizer/input.txt')))
+    global.startExecution();
+    if (logResult('Part 1 test', await part1(loadLines('05_Seed_Fertilizer/sampleData1.txt')), 35)) {
+        global.logExectionTime();
+
+        global.startExecution();
+        logResult('Part 1', await part1(loadLines('05_Seed_Fertilizer/input.txt')), 282277027);
+    }
+    global.logExectionTime();
+
 
     log();
 
-    if (logResult('Part 2 test', await part2(loadLines('05_Seed_Fertilizer/sampleData1.txt')), 0))
-        logResult('Part 2', await part2(loadLines('05_Seed_Fertilizer/input.txt')))
+    global.startExecution();
+    if (true || logResult('Part 2 test', await part2(loadLines('05_Seed_Fertilizer/sampleData1.txt')), 46)) {
+        global.logExectionTime();
 
-    log(colors.fg.gray + `Executed in ${(new Date().getTime() - startTime.getTime())}ms`);
+        global.startExecution();
+        if (logResult('Part 2 OE', await part2(loadLines('05_Seed_Fertilizer/input-oe.txt')), 79004094)) {
+            global.logExectionTime();
+
+            global.startExecution();
+            logResult('Part 2', await part2(loadLines('05_Seed_Fertilizer/input.txt')), (n) => n < 11554136);
+        }
+    }
+
+    global.logExectionTime();
 }
 
 async function part1(data: string[]): Promise<number> {
     const seeds = data.shift()!.split(': ')[1].split(' ').map(x => parseInt(x));
+
+    const { maps, route } = prepareMap(data);
+    const seedRoutes: { [key: number]: { [key: string]: number } } = {};
+
+    seeds.forEach(seed => {
+        let last = seed;
+        seedRoutes[seed] = {};
+
+        route.forEach(route => {
+            last = findValRoute(last, maps[route]);
+            seedRoutes[seed][route] = last;
+        });
+    });
+
+    return Math.min(...seeds.map(seed => seedRoutes[seed][route[route.length - 1]]));
+}
+
+async function part2(data: string[]): Promise<number> {
+    const seedsRanges = data.shift()!.split(': ')[1].split(' ').map(x => parseInt(x));
+    const newSeedRanges: { source: number, sourceMax: number, length: number }[] = [];
+
+    for (let i = 0; i < seedsRanges.length; i += 2) {
+        newSeedRanges.push({
+            source: seedsRanges[i],
+            sourceMax: seedsRanges[i] + seedsRanges[i + 1],
+            length: seedsRanges[i + 1]
+        });
+    }
+
+    const { maps, route } = prepareMap(data);
+    log(maps);
+    log(route);
+
+    let min = Infinity;
+    let i = 0;
+    for (let range of newSeedRanges) {
+        for (let seed = range.source; seed < range.sourceMax; seed++) {
+            const path = [];
+            let last = seed;
+            path.push(seed);
+            route.forEach(route => {
+                last = findValRoute(last, maps[route]);
+                path.push(last);
+            });
+
+            if (last < min)
+                min = last;
+        }
+
+        log(`Range ${++i}: ${min} ${colors.fg.gray} (${global.getExecutionTime()})`);
+    }
+    return min;
+}
+
+function prepareMap(data: string[]) {
     const maps: { [key: string]: Map[] } = {};
     const route: string[] = [];
 
@@ -46,23 +118,7 @@ async function part1(data: string[]): Promise<number> {
         });
     });
 
-    const seedRoutes: { [key: number]: { [key: string]: number } } = {};
-
-    seeds.forEach(seed => {
-        let last = seed;
-        seedRoutes[seed] = {};
-
-        route.forEach(route => {
-            last = findValRoute(last, maps[route]);
-            seedRoutes[seed][route] = last;
-        });
-    });
-
-    return Math.min(...seeds.map(seed => seedRoutes[seed][route[route.length - 1]]));
-}
-
-async function part2(data: string[]): Promise<number> {
-    return -Infinity;
+    return { maps, route };
 }
 
 type Map = { source: number, sourceMax: number, dest: number, length: number };
@@ -73,12 +129,13 @@ function findValRoute(number: number, maps: Map[]) {
         if (result !== null)
             return result;
     }
+
     return number;
 }
 
 function findVal(number: number, map: Map): number | null {
-    if (number < map.source || number > map.sourceMax)
-        return null;
+    if (number >= map.source && number < map.sourceMax)
+        return number - map.source + map.dest;
 
-    return number - map.source + map.dest;
+    return null;
 }
--- global.ts
+++ global.ts
@@ -2,8 +2,10 @@ import fs from 'fs';
 import { colors } from './types';
 import log from './log';
 
-export function logResult(title: string, result: number, expected?: number) {
-    const pass = result === expected;
+let startDate: Date;
+
+export function logResult(title: string, result: number, expected?: number | ((n: number) => boolean)) {
+    const pass = (typeof expected === 'function') ? expected(result) : result === expected;
 
     if (expected !== undefined)
         log((pass ? colors.fg.green : colors.fg.red) + `${title}: ${result} ${pass ? '===' : '!=='} ${expected} ${pass ? "PASS" : "FAIL"}`);
@@ -13,11 +15,39 @@ export function logResult(title: string, result: number, expected?: number) {
     return pass || expected === undefined;
 }
 
+export function startExecution() {
+    startDate = new Date();
+}
+
+export function logExectionTime() {
+    log(colors.fg.gray + `Executed in ${getExecutionTime()}`);
+}
+
+export function getExecutionTime() {
+    return `${(parseDuration(new Date().getTime() - startDate.getTime()))}`;
+}
+
 export function loadLines(path: string) {
-    return fs.readFileSync(path).toString().split('\r\n');
+    return fs.readFileSync(path).toString().split(/\r?\n/);
 }
 
 export function regexIndexOf(string: string, regex: RegExp, startpos?: number) {
     var indexOf = string.substring(startpos || 0).search(regex);
     return (indexOf >= 0) ? (indexOf + (startpos || 0)) : indexOf;
+}
+
+function parseDuration(duration: number) {
+    const seconds = Math.floor(duration) / 1000;
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+
+    if (hours >= 1) {
+        return `${hours}h ${minutes % 24}m ${seconds % 60}s`;
+    } else if (minutes >= 1) {
+        return `${minutes}m ${seconds % 60}s`;
+    } else if (seconds >= 10) {
+        return `${seconds}s`;
+    } else {
+        return `${duration}ms`;
+    }
 }
