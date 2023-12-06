5e69b59cf90b3f57607cb63ffdf2cdda599e9ffc
Milan Jelínek <mjelinek@artex-is.cz>
2023-12-06 21:20:45 +0100

    Day 6: Wait For It part 2

--- 06_Wait_For_It/waitforit.ts
+++ 06_Wait_For_It/waitforit.ts
@@ -7,12 +7,12 @@ export default async function main() {
     log("Day 6: Wait For It");
 
     if (logResult('Part 1 test', await part1(loadLines('06_Wait_For_It/sampleData1.txt')), 288))
-        logResult('Part 1', await part1(loadLines('06_Wait_For_It/input.txt')))
+        logResult('Part 1', await part1(loadLines('06_Wait_For_It/input.txt')), 1159152)
 
     log();
 
-    if (logResult('Part 2 test', await part2(loadLines('06_Wait_For_It/sampleData1.txt')), 0))
-        logResult('Part 2', await part2(loadLines('06_Wait_For_It/input.txt')))
+    if (logResult('Part 2 test', await part2(loadLines('06_Wait_For_It/sampleData1.txt')), 71503))
+        logResult('Part 2', await part2(loadLines('06_Wait_For_It/input.txt')), 41513103)
 
     log(colors.fg.gray + `Executed in ${(new Date().getTime() - startTime.getTime())}ms`);
 }
@@ -32,6 +32,17 @@ async function part1(data: string[]): Promise<number> {
         });
     }
 
+    return part0(races);
+}
+
+async function part2(data: string[]): Promise<number> {
+    return part0([{
+        time: parseInt(data[0].split(/ +/).slice(1).join('')),
+        target: parseInt(data[1].split(/ +/).slice(1).join(''))
+    }]);
+}
+
+async function part0(races: { time: number, target: number }[]) {
     let sum = 1;
 
     for (const race of races) {
@@ -44,8 +55,4 @@ async function part1(data: string[]): Promise<number> {
     }
 
     return sum;
-}
-
-async function part2(data: string[]): Promise<number> {
-    return -Infinity;
 }
