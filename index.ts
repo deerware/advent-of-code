536fa459d2491314769d452ea75be1c02538866b
Milan Jelínek <mjelinek@artex-is.cz>
2023-12-08 10:56:18 +0100

    Day 8: Haunted Wasteland part 1 + 2

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ import scratchcards from './04_Scratchcards/scratchcards';
 import seedfertilizer from './05_Seed_Fertilizer/seedfertilizer';
 import waitforit from './06_Wait_For_It/waitforit';
 import camelcards from './07_Camel_Cards/camelcards';
+import hauntedWasteland from './08_Haunted_Wasteland/hauntedwasteland';
 import log from './log';
 
 (async () => {
@@ -15,7 +16,8 @@ import log from './log';
         // await scratchcards();
         // await seedfertilizer();
         // await waitforit();
-        await camelcards();
+        // await camelcards();
+        await hauntedWasteland();
     } catch (e) {
         log('Error:', (e as any).message);
     }
