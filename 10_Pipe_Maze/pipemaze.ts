2bf8ae5a639fd73216a2d782f7abdaf94f9d4ab1
Milan Jelínek <mjelinek@artex-is.cz>
2023-12-10 12:22:31 +0100

    Day 10: Pipe Maze part 2 draft

--- 10_Pipe_Maze/pipemaze.ts
+++ 10_Pipe_Maze/pipemaze.ts
@@ -7,7 +7,7 @@ export default async function main() {
     log("Day 10: Pipe Maze");
 
     global.startExecution();
-    if (logResult('Part 1 test 4', await part1(loadLines('10_Pipe_Maze/sampleData1.txt')), 4)) {
+    if (logResult('Part 1 test 1', await part1(loadLines('10_Pipe_Maze/sampleData1.txt')), 4)) {
         global.logExectionTime();
 
         global.startExecution();
@@ -15,33 +15,42 @@ export default async function main() {
             global.logExectionTime();
 
             global.startExecution();
-            logResult('Part 1', await part1(loadLines('10_Pipe_Maze/input.txt')))
+            logResult('Part 1', await part1(loadLines('10_Pipe_Maze/input.txt')), 7173)
         }
     }
     global.logExectionTime();
 
-    // log();
+    log();
 
-    // global.startExecution();
-    // if (logResult('Part 2 test', await part2(loadLines('10_Pipe_Maze/sampleData3.txt')), 0)) {
-    //     global.logExectionTime();
+    global.startExecution();
+    if (logResult('Part 2 test 1', await part2(loadLines('10_Pipe_Maze/sampleData3.txt')), 8)) {
+        global.logExectionTime();
 
-    //     global.startExecution();
-    //     logResult('Part 2', await part2(loadLines('10_Pipe_Maze/input.txt')))
-    // }
-    // global.logExectionTime();
+        global.startExecution();
+        if (logResult('Part 1 test 2', await part2(loadLines('10_Pipe_Maze/sampleData4.txt')), 10)) {
+            global.logExectionTime();
+
+            global.startExecution();
+            logResult('Part 2', await part1(loadLines('10_Pipe_Maze/input.txt')))
+        }
+    }
+    global.logExectionTime();
 }
 
 type Pos = { l: number, c: number };
 
-async function part1(data: string[]): Promise<number> {
+async function part1(data: string[], maze: Pos[] = []): Promise<number> {
     const start = findStart(data);
     const connected = startLeadsTo(data, start);
+
 
     let p1 = start;
     let p2 = start;
     let c1 = connected[0];
     let c2 = connected[1];
+
+    maze.push(start, c1, c2);
+
     let steps = 1;
     while (true) {
         steps++;
@@ -49,27 +58,38 @@ async function part1(data: string[]): Promise<number> {
         const n1 = leadsTo1(data[c1.l][c1.c], c1, p1);
         const n2 = leadsTo1(data[c2.l][c2.c], c2, p2);
 
-        if (n1.l === n2.l && n1.c === n2.c)
+        if (n1.l === n2.l && n1.c === n2.c) {
+            maze.push(n1);
             return steps;
+        }
 
         p1 = c1;
         p2 = c2;
         c1 = n1;
         c2 = n2;
+
+        maze.push(c1);
+        maze.push(c2);
+    }
+}
+
+async function part2(data: string[]): Promise<number> {
+    const maze: Pos[] = [];
+    part1(data, maze);
+
+    const prev: Pos[] = [];
+    for (const pos of maze) {
+        const enclosed = encloses(data[pos.l][pos.c], maze, pos);
     }
 
     return -Infinity;
 }
 
-async function part2(data: string[]): Promise<number> {
-    return -Infinity;
-}
-
 function findStart(data: string[]): Pos {
-    for (let l in data) {
+    for (let l = 0; l < data.length; l++) {
         for (let c = 0; c < data[l].length; c++) {
             if (data[l][c] === 'S')
-                return { l: parseInt(l), c: parseInt(c) };
+                return { l, c };
         }
     }
     throw new Error('Start not found');
@@ -92,7 +112,7 @@ function startLeadsTo(data: string[], start: Pos): [Pos, Pos] {
     if (connected.length !== 2)
         throw new Error(`Start has ${connected.length} connections`);
 
-    return connected;
+    return connected as [Pos, Pos];
 }
 
 function leadsTo(symbol: string, pos: Pos, prev: Pos): Pos[] {
@@ -124,10 +144,133 @@ function connects(symbol: string, pos: Pos): [Pos, Pos] | [] {
         case 'F':
             return [{ l: pos.l + 1, c: pos.c }, { l: pos.l, c: pos.c + 1 }];
         case 'S':
-            log('Warning! Found S!');
         case '.':
             return [];
     }
 
     throw new Error(`Unknown symbol: ${symbol}`);
 }
+
+function encloses(symbol: string, maze: Pos[], pos: Pos) {
+    let result: [Pos[], Pos[]];
+
+    switch (symbol) {
+        case '|':
+            result = [
+                [
+                    { l: pos.l, c: pos.c - 1 },
+                ],
+                [
+                    { l: pos.l, c: pos.c + 1 },
+                ]
+            ]
+            break;
+        case '-':
+            result = [
+                [
+                    { l: pos.l - 1, c: pos.c },
+                ],
+                [
+                    { l: pos.l + 1, c: pos.c },
+                ]
+            ]
+            break;
+        case 'L':
+            result = [
+                [
+                    { l: pos.l - 1, c: pos.c + 1 },
+                ],
+                [
+                    { l: pos.l, c: pos.c - 1 },
+                    { l: pos.l + 1, c: pos.c },
+                ]
+            ];
+            break;
+        case 'J':
+            result = [
+                [
+                    { l: pos.l - 1, c: pos.c - 1 },
+                ],
+                [
+                    { l: pos.l, c: pos.c + 1 },
+                    { l: pos.l + 1, c: pos.c },
+                ]
+            ];
+            break;
+        case '7':
+            result = [
+                [
+                    { l: pos.l + 1, c: pos.c - 1 },
+                ],
+                [
+                    { l: pos.l, c: pos.c + 1 },
+                    { l: pos.l - 1, c: pos.c },
+                ]
+            ];
+            break;
+        case 'F':
+            result = [
+                [
+                    { l: pos.l + 1, c: pos.c + 1 },
+                ],
+                [
+                    { l: pos.l, c: pos.c - 1 },
+                    { l: pos.l - 1, c: pos.c },
+                ]
+            ];
+            break;
+        case 'S':
+        case '.':
+            return [[], []];
+            break;
+        default:
+            throw new Error(`Unknown symbol: ${symbol}`);
+    }
+
+    return [
+        result[0].filter(x => !maze.some(y => y.l === x.l && y.c === x.c)),
+        result[1].filter(x => !maze.some(y => y.l === x.l && y.c === x.c))
+    ];
+}
+
+
+// function findBounds(maze: Pos[]) {
+//     let minL = Infinity;
+//     let minC = Infinity;
+//     let maxL = -Infinity;
+//     let maxC = -Infinity;
+
+//     for (const pos of maze) {
+//         if (pos.l < minL)
+//             minL = pos.l;
+//         if (pos.c < minC)
+//             minC = pos.c;
+//         if (pos.l > maxL)
+//             maxL = pos.l;
+//         if (pos.c > maxC)
+//             maxC = pos.c;
+//     }
+
+//     return { min: { l: minL, c: minC }, max: { l: maxL, c: maxC } };
+// }
+
+// function findEligible(data: string[], maze: Pos[], bounds: { min: Pos, max: Pos }) {
+//     const eligible: Pos[] = [];
+
+//     for (let l = bounds.min.l; l < bounds.max.l; l++) {
+//         for (let c = bounds.min.c; c < bounds.max.c; c++) {
+//             if (data[l][c] !== '.')
+//                 continue;
+
+//             if (
+//                 maze.some(x => x.l < l) &&
+//                 maze.some(x => x.l > l) &&
+//                 maze.some(x => x.c < l) &&
+//                 maze.some(x => x.c > l)
+//             )
+//                 eligible.push({ l, c });
+//         }
+//     }
+
+//     return eligible;
+// }
