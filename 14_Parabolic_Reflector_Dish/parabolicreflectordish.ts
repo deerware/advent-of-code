7eb6f90a8fba8867b3431b17fb6693efc18953e8
Milan Jelínek <mjelinek@artex-is.cz>
2023-12-14 23:11:04 +0100

    Day 14: Parabolic Reflector Dish part 2

--- 14_Parabolic_Reflector_Dish/parabolicreflectordish.ts
+++ 14_Parabolic_Reflector_Dish/parabolicreflectordish.ts
@@ -7,23 +7,48 @@ export default async function main() {
 
     global.run('14_Parabolic_Reflector_Dish', [
         ['Part 1 test 1', part1, 'sampleData1.txt', 136],
-        ['Part 1', part1, 'input.txt', null],
+        ['Part 1', part1, 'input.txt', 108813],
         null,
-        ['Part 2 test 1', part2, 'sampleData2.txt', 64],
-        ['Part 2', part2, 'input.txt', null],
+        ['Part 2 test 1', part2, 'sampleData1.txt', 64],
+        ['Part 2', part2, 'input.txt', 104533],
     ]);
 }
 
 async function part1(data: string[]): Promise<number> {
     const map = parseData(data);
     translateNorth(map);
-    // drawMap(map, data.length, data[0].length);
 
     return calcWeight(map, data.length);
 }
 
 async function part2(data: string[]): Promise<number> {
-    return -Infinity;
+    const map = parseData(data);
+
+    const maxL = data.length;
+    const maxC = data[0].length;
+    const cycles = 1000000000;
+
+    let match: number | undefined;
+    const cycleHistory: string[] = [];
+    for (let i = 0; i < cycles; i++) {
+        translationCycle(map, maxL, maxC);
+        const hash = getHash(map);
+        if (cycleHistory.includes(hash)) {
+            match = cycleHistory.indexOf(hash);
+            break;
+        }
+        cycleHistory.push(hash);
+    }
+
+    if (match !== undefined) {
+        const pattern = cycleHistory.length - match;
+        const fullCycles = Math.floor((cycles - match) / pattern) * pattern + match + 1;
+
+        for (let i = fullCycles; i < cycles; i++)
+            translationCycle(map, maxL, maxC);
+    }
+
+    return calcWeight(map, data.length);
 }
 
 type Pos = { l: number, c: number };
@@ -45,14 +70,15 @@ function parseData(data: string[]) {
     return { stones, balls };
 }
 
-// function translationCycle(map: Map) {
-//     translateNorth(map);
-//     translateWest(map);
-//     translateSouth(map);
-//     translateEast(map);
-// }
+function translationCycle(map: Map, maxL: number, maxC: number) {
+    translateNorth(map);
+    translateWest(map);
+    translateSouth(map, maxL);
+    translateEast(map, maxC);
+}
 
 function translateNorth(map: Map) {
+    sortBalls(map);
     for (const ball of map.balls) {
         for (let l = ball.l - 1; l >= 0; l--) {
             if (map.stones.some(stone => stone.l === l && stone.c === ball.c))
@@ -65,9 +91,24 @@ function translateNorth(map: Map) {
     }
 }
 
-function translateEast(map: Map) {
+function translateWest(map: Map) {
+    sortBalls(map);
     for (const ball of map.balls) {
-        for (let l = ball.l - 1; l >= 0; l--) {
+        for (let c = ball.c - 1; c >= 0; c--) {
+            if (map.stones.some(stone => stone.c === c && stone.l === ball.l))
+                break;
+            if (map.balls.some(otherBall => otherBall.c === c && otherBall.l === ball.l))
+                break;
+
+            ball.c = c;
+        }
+    }
+}
+
+function translateSouth(map: Map, maxL: number) {
+    sortBalls(map, -1);
+    for (const ball of map.balls) {
+        for (let l = ball.l + 1; l < maxL; l++) {
             if (map.stones.some(stone => stone.l === l && stone.c === ball.c))
                 break;
             if (map.balls.some(otherBall => otherBall.l === l && otherBall.c === ball.c))
@@ -76,6 +117,37 @@ function translateEast(map: Map) {
             ball.l = l;
         }
     }
+}
+
+function translateEast(map: Map, maxC: number) {
+    sortBalls(map, -1);
+    for (const ball of map.balls) {
+        for (let c = ball.c + 1; c < maxC; c++) {
+            if (map.stones.some(stone => stone.c === c && stone.l === ball.l))
+                break;
+            if (map.balls.some(otherBall => otherBall.c === c && otherBall.l === ball.l))
+                break;
+
+            ball.c = c;
+        }
+    }
+}
+
+function sortBalls(map: Map, invert = 1) {
+    map.balls.sort((a, b) => {
+        if (a.l != b.l)
+            return invert * (a.l - b.l);
+
+        return invert * (a.c - b.c);
+    });
+}
+
+function getHash(map: Map) {
+    let hash = "";
+    for (const ball of map.balls) {
+        hash += `l${ball.l}c${ball.c}`;
+    }
+    return hash;
 }
 
 function calcWeight(map: Map, maxL: number) {
