a83280432c5569307979760a1650b1744b227d23
Milan Jelínek <mjelinek@artex-is.cz>
2023-12-15 08:00:53 +0100

    Day 15: Lens Library part 1

--- 15_Lens_Library/lenslibrary.ts
+++ 15_Lens_Library/lenslibrary.ts
@@ -6,7 +6,8 @@ export default async function main() {
     log('Day 15: Lens Library');
 
     global.run('15_Lens_Library', [
-        ['Part 1 test 1', part1, 'sampleData1.txt', 1320],
+        ['Part 1 test 1', part1, 'sampleData1.txt', 52],
+        ['Part 1 test 2', part1, 'sampleData2.txt', 1320],
         ['Part 1', part1, 'input.txt', null],
         null,
         ['Part 2 test 1', part2, 'sampleData2.txt', 0],
@@ -15,9 +16,27 @@ export default async function main() {
 }
 
 async function part1(data: string[]): Promise<number> {
-    return -Infinity;
+    if (data.length > 1)
+        throw new Error('Invalid data input');
+
+    let sum = 0;
+    const parts = data[0].split(',');
+    for (const part of parts) {
+        sum += hash(part);
+    }
+    return sum;
 }
 
 async function part2(data: string[]): Promise<number> {
+    if (data.length > 1)
+        throw new Error('Invalid data input');
     return -Infinity;
+}
+
+function hash(text: string) {
+    let currentValue = 0;
+    for (let i = 0; i < text.length; i++) {
+        currentValue = (currentValue + text.charCodeAt(i)) * 17 % 256
+    }
+    return currentValue;
 }
