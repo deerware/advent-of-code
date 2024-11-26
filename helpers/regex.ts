1068da8f54ae62305dee97e02bd2c1dab88c1a51
Milan Jelínek <mjelinek@artex-is.cz>
2024-11-26 23:27:52 +0100

    regex helper

--- helpers/regex.ts
+++ helpers/regex.ts
@@ -1,5 +1,38 @@
 export default {
     allMatches(search: string, exp: RegExp) {
-        console.log(search.matchAll(exp));
+        const matches = [];
+        let match;
+        while ((match = exp.exec(search)) !== null) {
+            matches.push(match[0]);
+        }
+        return matches;
+    },
+    allMatchesG(search: string, exp: RegExp) {
+        const matches = [];
+        let match: RegExpExecArray | null;
+        while ((match = exp.exec(search)) !== null) {
+            matches.push({
+                full: match.shift(),
+                groups: [...match],
+            });
+        }
+        return matches;
+    },
+    firstMatch(search: string, exp: RegExp) {
+        let match: RegExpExecArray | null;
+        while ((match = exp.exec(search)) !== null) {
+            return match[0];
+        }
+        return null;
+    },
+    firstMatchG(search: string, exp: RegExp) {
+        let match: RegExpExecArray | null;
+        while ((match = exp.exec(search)) !== null) {
+            return {
+                full: match.shift(),
+                groups: [...match],
+            }
+        }
+        return null;
     }
 }
