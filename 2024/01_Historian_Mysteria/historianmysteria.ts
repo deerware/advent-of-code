e72769bedc6e3c0e6a468a092f4e1bcabdefe54a
Milan Jelínek <mjelinek@artex-is.cz>
2024-12-01 06:19:22 +0100

    Day 1: Historian Hysteria part 1

---
+++ 2024/01_Historian_Mysteria/historianmysteria.ts
@@ -0,0 +1,39 @@
+import log from '../../log'
+import { colors } from '../../types'
+import * as global from '../../global';
+
+export default async function main() {
+    log('Day 1: Historian Hysteria');
+
+    global.run('2024/01_Historian_Mysteria', [
+        ['Part 1 test 1', part1, 'sampleData1.txt', 11],
+        ['Part 1', part1, 'input.txt', null],
+        null,
+        ['Part 2 test 1', part2, 'sampleData2.txt', 0],
+        ['Part 2', part2, 'input.txt', null],
+    ]);
+}
+
+async function part1(data: string[]): Promise<number> {
+    const left = [];
+    const right = [];
+    for (const line of data) {
+        const curr = line.split(' ');
+        left.push(parseInt(curr[0]));
+        right.push(parseInt(curr[curr.length - 1]));
+    }
+    left.sort();
+    right.sort();
+    if (left.length !== right.length)
+        throw new Error('Lengths do not match');
+
+    let sum = 0;
+    for (let i = 0; i < left.length; i++) {
+        sum += Math.abs(left[i] - right[i]);
+    }
+    return sum;
+}
+
+async function part2(data: string[]): Promise<number> {
+    return -Infinity;
+}
--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 import log from './log';
+import historianmysteria from './2024/01_Historian_Mysteria/historianmysteria';
 
 (async () => {
     try {
-
+        await historianmysteria();
     } catch (e) {
         log('Error:', (e as any).message);
     }
