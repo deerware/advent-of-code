af6fa71f434920e6826da6a0a7b5bd8807a9de54
Milan Jelínek <mjelinek@artex-is.cz>
2024-12-02 10:12:24 +0100

    Day 2: Red-Nosed Reports part 2

--- 2024/02_Red_Nosed_Reports/rednosedreports.ts
+++ 2024/02_Red_Nosed_Reports/rednosedreports.ts
@@ -5,12 +5,12 @@ import * as global from '../../global';
 export default async function rednosedreports() {
     log('Day 2: Red-Nosed Reports');
 
-    global.run('2024/02_Red_Nosed_Reports', [
+    await global.run('2024/02_Red_Nosed_Reports', [
         ['Part 1 test 1', part1, 'sampleData1.txt', 2],
         ['Part 1', part1, 'input.txt', 598],
         null,
-        ['Part 2 test 1', part2, 'sampleData1.txt', 0],
-        ['Part 2', part2, 'input.txt', null],
+        ['Part 2 test 1', part2, 'sampleData1.txt', 4],
+        ['Part 2', part2, 'input.txt', 634],
     ]);
 }
 
@@ -24,31 +24,63 @@ async function part1(_data: string[]): Promise<number> {
     return count;
 }
 
-async function part2(data: string[]): Promise<number> {
-    return -Infinity;
+async function part2(_data: string[]): Promise<number> {
+    const data = _data.map(line => line.split(' ').map(Number));
+    let count = 0;
+    for (const line of data) {
+        if (safe(line, true))
+            count++;
+    }
+    return count;
+
 }
 
-function safe(data: number[]) {
+function safe(data: number[], allowSingleError = false): boolean {
     let num;
     let dir;
-    for (const n of data) {
+    for (let i = 0; i < data.length; i++) {
+        const n = data[i];
+
         if (num === undefined) {
             num = n;
             continue;
         }
         if (n === num)
-            return false;
+            if (allowSingleError)
+                return tryWithoutOne(data);
+            else
+                return false;
         if (dir === undefined) {
             dir = n > num;
         } else {
             if (dir !== n > num)
-                return false;
+                if (allowSingleError)
+                    return tryWithoutOne(data);
+                else
+                    return false;
         }
 
         if (Math.abs(n - num) > 3)
-            return false;
+            if (allowSingleError)
+                return tryWithoutOne(data);
+            else
+                return false;
 
         num = n;
     }
     return true;
+}
+
+function tryWithoutOne(arr: number[]) {
+    for (let i = 0; i < arr.length; i++) {
+        if (safe(removeItemAt(arr, i)))
+            return true;
+    }
+    return false;
+}
+
+function removeItemAt<T>(arr: T[], at: number) {
+    const temp = [...arr];
+    temp.splice(at, 1);
+    return temp;
 }
