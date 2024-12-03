61a987f7f32eb753aacd5dc64bbd6df6b08fa4a0
Milan Jelínek <mjelinek@artex-is.cz>
2024-12-03 06:11:42 +0100

    Day 3: Mull It Over part 2

--- 2024/03_Mull_It_Over/mullitover.ts
+++ 2024/03_Mull_It_Over/mullitover.ts
@@ -8,10 +8,10 @@ export default async function mullitover() {
 
     await global.run('2024/03_Mull_It_Over', [
         ['Part 1 test 1', part1, 'sampleData1.txt', 161],
-        ['Part 1', part1, 'input.txt', null],
+        ['Part 1', part1, 'input.txt', 188741603],
         null,
-        ['Part 2 test 1', part2, 'sampleData2.txt', 0],
-        ['Part 2', part2, 'input.txt', null],
+        ['Part 2 test 1', part2, 'sampleData2.txt', 48],
+        ['Part 2', part2, 'input.txt', 67269798],
     ]);
 }
 
@@ -26,6 +26,20 @@ async function part1(_data: string[]): Promise<number> {
     return sum;
 }
 
-async function part2(data: string[]): Promise<number> {
-    return -Infinity;
+async function part2(_data: string[]): Promise<number> {
+    const data = _data.join('\n');
+
+    let sum = 0;
+    let enabled = true;
+
+    for (const match of regex.allMatchesG(data, /(do)\(\)|(don't)\(\)|(mul)\((\d{1,3}),(\d{1,3})\)/gm)) {
+        if (match.groups[0] === 'do')
+            enabled = true;
+        if (match.groups[1] === "don't")
+            enabled = false;
+        if (match.groups[2] === 'mul' && enabled)
+            sum += parseInt(match.groups[3]) * parseInt(match.groups[4]);
+    }
+
+    return sum;
 }
