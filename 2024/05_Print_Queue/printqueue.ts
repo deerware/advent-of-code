ce71a1d12518e07269d04851d931b0d2b74da0f7
Milan Jelínek <mjelinek@artex-is.cz>
2024-12-05 06:26:33 +0100

    Day 5: Print Queue part 2

--- 2024/05_Print_Queue/printqueue.ts
+++ 2024/05_Print_Queue/printqueue.ts
@@ -10,7 +10,7 @@ export default async function printqueue() {
         ['Part 1', part1, 'input.txt', 6951],
         null,
         ['Part 2 test 1', part2, 'sampleData.txt', 123],
-        ['Part 2', part2, 'input.txt', null],
+        ['Part 2', part2, 'input.txt', 4121],
     ]);
 }
 
@@ -37,34 +37,61 @@ function parseData(data: string[]) {
 
 async function part1(_data: string[]): Promise<number> {
     const data = parseData(_data);
-    const validUpdates = [];
 
-    for (const update of data.updates) {
-        let invalid = false;
-        for (const n of update) {
-            const rules = data.rules.filter(([first, second]) => n == first);
-            if (rules.length === 0)
-                continue;
-
-            for (const rule of rules) {
-                if (!update.includes(rule[1]))
-                    continue;
-
-                if (update.indexOf(rule[1]) < update.indexOf(rule[0])) {
-                    invalid = true;
-                    break;
-                }
-            }
-            if (invalid)
-                break;
-        }
-        if (!invalid)
-            validUpdates.push(update);
-    }
-
-    return validUpdates.reduce((acc, val) => acc += val[(val.length - 1) / 2], 0);
+    return data.updates
+        .filter(update => isValid(update, data.rules))
+        .reduce((acc, val) => acc += val[(val.length - 1) / 2], 0);
 }
 
-async function part2(data: string[]): Promise<number> {
-    return -Infinity;
+async function part2(_data: string[]): Promise<number> {
+    const data = parseData(_data);
+    const invalidUpdates = [];
+
+    for (const update of data.updates) {
+        if (!isValid(update, data.rules))
+            invalidUpdates.push(update);
+    }
+
+    for (const update of invalidUpdates) {
+        while (!isValid(update, data.rules)) {
+            for (const n of update) {
+                const rules = data.rules.filter(([first, second]) => n == first);
+                for (const rule of rules) {
+                    if (!update.includes(rule[1]))
+                        continue;
+
+                    const currPos = update.indexOf(rule[0]);
+                    const mustBeBeforePos = update.indexOf(rule[1]);
+                    if (currPos > mustBeBeforePos)
+                        move(update, currPos, mustBeBeforePos);
+                }
+            }
+        }
+    }
+
+    return invalidUpdates.reduce((acc, val) => acc += val[(val.length - 1) / 2], 0);
+}
+
+function isValid(update: number[], allRules: [number, number][]) {
+    for (const n of update) {
+        const rules = allRules.filter(([first, second]) => n == first);
+        if (rules.length === 0)
+            continue;
+
+        for (const rule of rules) {
+            if (!update.includes(rule[1]))
+                continue;
+
+            if (update.indexOf(rule[1]) < update.indexOf(rule[0])) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+function move<T>(input: T[], from: number, to: number) {
+    const elm = input.splice(from, 1)[0];
+    input.splice(to, 0, elm);
 }
