6d3f8fb740261c87566789984368518b9c870a4d
Milan Jelínek <mjelinek@artex-is.cz>
2024-12-11 08:28:30 +0100

    Day 11: Plutonian Pebbles - part 2

--- 2024/11_Plutonian_Pebbles/plutonianpebbles.ts
+++ 2024/11_Plutonian_Pebbles/plutonianpebbles.ts
@@ -7,11 +7,10 @@ export default async function plutonianpebbles() {
 
     await global.run('2024/11_Plutonian_Pebbles', [
         ['Part 1 test 1', part1, 'sampleData1.txt', 7, 1],
-        ['Part 1 test 1', part1, 'sampleData2.txt', 55312],
-        ['Part 1', part1, 'input.txt', null],
-        false,
-        ['Part 2 test 1', part2, 'sampleData2.txt', 0],
-        ['Part 2', part2, 'input.txt', null],
+        ['Part 1 test 2', part1, 'sampleData2.txt', 55312],
+        ['Part 1', part1, 'input.txt', 224529],
+        null,
+        ['Part 2', part1, 'input.txt', 266820198587914, 75],
     ], parseData);
 }
 
@@ -21,34 +20,65 @@ function parseData(_data: string[]) {
 }
 
 async function part1(data: Data, blinktimes = 25): Promise<number> {
-    for (let i = 0; i < blinktimes; i++) {
-        blink(data);
+    return data.reduce(
+        (acc, stone) =>
+            acc + count(stone, blinktimes), 0);
+}
+
+const cache: { [key: string]: number } = {};
+function count(stone: number, step: number): number {
+    const key = `${stone}-${step}`;
+    if (cache[key])
+        return cache[key];
+
+    const result = _count(stone, step);
+    cache[key] = result;
+    return result;
+}
+
+function _count(stone: number, blink: number): number {
+    const stoneStr = stone.toString();
+    const nextB = blink - 1;
+
+    // if (blink % 15 == 0)
+    //     log(`stone: ${stone}, blink: ${blink}, nextB: ${nextB}`);
+
+    if (stone === 0)
+        return blink === 1 ? 1 : count(1, nextB);
+
+    if (stoneStr.length % 2 === 0) {
+        const left = parseInt(stoneStr.slice(0, stoneStr.length / 2));
+        const right = parseInt(stoneStr.slice(stoneStr.length / 2));
+        return blink === 1 ? 2 : count(left, nextB) + count(right, nextB);
     }
-    return data.length;
+
+    return blink === 1 ? 1 : count(stone * 2024, nextB);
 }
 
-async function part2(data: Data): Promise<number> {
-    return -Infinity;
-}
+// function blink(data: Data) {
+//     for (let i = 0; i < data.length; i++) {
+//         const stone = data[i];
+//         const stoneStr = stone.toString();
+//         let addedTwo = false;
 
-function blink(data: Data) {
-    for (let i = 0; i < data.length; i++) {
-        const stone = data[i];
+//         switch (true) {
+//             case stone === 0:
+//                 data[i] = 1;
+//                 break;
+//             case stoneStr.length % 2 === 0:
+//                 const left = parseInt(stoneStr.slice(0, stoneStr.length / 2));
+//                 const right = parseInt(stoneStr.slice(stoneStr.length / 2));
+//                 data[i] = left;
+//                 data.splice(++i, 0, right); // ++i -> skip the newly added stone
+//                 addedTwo = true;
+//                 break;
+//             default:
+//                 data[i] *= 2024;
+//                 break;
+//         }
 
-        if (stone === 0) {
-            data[i] = 1;
-            continue;
-        }
-
-        const stoneStr = stone.toString();
-        if (stoneStr.length % 2 === 0) {
-            const left = stoneStr.slice(0, stoneStr.length / 2);
-            const right = stoneStr.slice(stoneStr.length / 2);
-            data[i] = parseInt(left);
-            data.splice(++i, 0, parseInt(right)); // ++i -> skip the newly added stone
-            continue;
-        }
-
-        data[i] *= 2024;
-    }
-}
+//         uniqueNums.add(data[i]);
+//         if (addedTwo)
+//             uniqueNums.add(data[i + 1]);
+//     }
+// }
