1804c9b6e01f44fc501304505b6aa30de8d99852
Milan Jelínek <mjelinek@artex-is.cz>
2024-12-13 07:23:55 +0100

    Day 13: Claw Contraption - part 2

--- 2024/13_Claw_Contraption/clawcontraption.ts
+++ 2024/13_Claw_Contraption/clawcontraption.ts
@@ -2,11 +2,18 @@ import log from '../../log'
 import { colors } from '../../types'
 import * as global from '../../global';
 import * as mathjs from 'mathjs';
+import Decimal from 'decimal.js';
+
+const math = mathjs.create(mathjs.all, {
+    number: 'BigNumber',
+    precision: 256,
+});
 
 // Inspiration for today:
 // https://www.wolframalpha.com/calculators/system-equation-calculator
 // https://www.geeksforgeeks.org/how-to-solve-equations-with-mathjs/
 // https://mathjs.org/docs/reference/functions/lusolve.html
+// https://mathjs.org/docs/datatypes/bignumbers.html
 
 export default async function clawcontraption() {
     log('Day 13: Claw Contraption');
@@ -15,8 +22,9 @@ export default async function clawcontraption() {
         ['Part 1 test 1', part1, 'sampleData.txt', 480],
         ['Part 1', part1, 'input.txt', 32067],
         null,
-        ['Part 2 test 1', part2, 'sampleData.txt', null],
-        ['Part 2', part2, 'input.txt', null],
+        ['Part 2 test 1', part2, 'sampleData.txt', 875318608908],
+        false,
+        ['Part 2', part2, 'input.txt', 92871736253789],
     ], parseData);
 }
 
@@ -54,24 +62,26 @@ async function part2(data: Data): Promise<number> {
     return part1(data, true);
 }
 
-const ff = Math.pow(10, 12); // floatingFix
+const ff = Math.pow(10, 4); // no ideal, sadly
 const part2Offset = 10000000000000;
 function findPrize(arcade: Arcade, part2 = false) {
-    const solution = mathjs.lusolve(
+    const prizeX = math.bignumber(arcade.prize[0]).add(part2 ? part2Offset : 0);
+    const prizeY = math.bignumber(arcade.prize[1]).add(part2 ? part2Offset : 0);
+    const solution = math.lusolve(
         [
-            [arcade.buttonA[0], arcade.buttonB[0]],
-            [arcade.buttonA[1], arcade.buttonB[1]],
+            [math.bignumber(arcade.buttonA[0]), math.bignumber(arcade.buttonB[0])],
+            [math.bignumber(arcade.buttonA[1]), math.bignumber(arcade.buttonB[1])],
         ],
-        [arcade.prize[0] + (part2 ? part2Offset : 0), arcade.prize[1] + (part2 ? part2Offset : 0)]
+        [prizeX, prizeY]
     ) as number[][];
 
     const aCount = Math.round(solution[0][0] * ff) / ff;
     const bCount = Math.round(solution[1][0] * ff) / ff;
 
-    if (aCount < 0 || bCount < 0)
-        return null;
-
-    if (!Number.isInteger(aCount) || !Number.isInteger(bCount))
+    if (
+        aCount < 0 || bCount < 0 ||
+        !Number.isInteger(aCount) || !Number.isInteger(bCount)
+    )
         return null;
 
     return [aCount, bCount];
