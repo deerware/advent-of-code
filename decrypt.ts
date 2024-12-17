b73e0982147df0e695426854d2b621c096e7717b
Milan Jelínek <mjelinek@artex-is.cz>
2024-12-17 21:19:16 +0100

    encrypt/decrypt in place

--- decrypt.ts
+++ decrypt.ts
@@ -11,7 +11,5 @@ const args = z.tuple([
     z.string(),
 ]).parse(process.argv);
 
-log(args[2]);
-
 const data = fs.readFileSync(args[2], 'utf-8');
-fs.writeFileSync(args[3], inputCrypt.decrypt(data));
+fs.writeFileSync(args[3] == "." ? args[2] : args[3], inputCrypt.decrypt(data));
--- encrypt.ts
+++ encrypt.ts
@@ -12,4 +12,4 @@ const args = z.tuple([
 ]).parse(process.argv);
 
 const data = fs.readFileSync(args[2], 'utf-8');
-fs.writeFileSync(args[3], inputCrypt.encrypt(data));
+fs.writeFileSync(args[3] == "." ? args[2] : args[3], inputCrypt.encrypt(data));
