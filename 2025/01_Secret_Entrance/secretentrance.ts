40865e4dceb33ee9f9d51805348b9c1b011566f5
Milan Jelínek <mjelinek59@gmail.com>
2025-12-01 11:04:34 +0100

    2025 Day 1: Secret Entrance part 2

--- 2025/01_Secret_Entrance/secretentrance.ts
+++ 2025/01_Secret_Entrance/secretentrance.ts
@@ -8,9 +8,10 @@ export default async function secretentrance() {
     await g.run('2025/01_Secret_Entrance', [
         ['Part 1 test 1', part1, 'sampleData1.txt', 3],
         ['Part 1', part1, 'input.txt', 997],
-        false,
-        ['Part 2 test 1', part2, 'sampleData2.txt', 0],
-        ['Part 2', part2, 'input.txt', null],
+        null,
+        ['Part 2 test 1', part2, 'sampleData1.txt', 6],
+        ['Part 2 test 2', part2, 'sampleData2.txt', 2],
+        ['Part 2', part2, 'input.txt', 5978],
     ], parseData);
 }
 
@@ -20,26 +21,45 @@ function parseData(_data: string[]) {
 }
 
 async function part1(data: Data): Promise<number> {
-    let pos = 50;
-    let hits = 0;
-
-    for (const line of data) {
-        if (line.dir == 'R')
-            pos += line.dist;
-        else
-            pos -= line.dist;
-
-        pos %= 100;
-        if (pos < 0)
-            pos += 100;
-
-        if (pos === 0)
-            hits++;
-    }
-
-    return hits;
+    return await part0(data);
 }
 
 async function part2(data: Data): Promise<number> {
-    return -Infinity;
+    return await part0(data, true);
+}
+
+async function part0(data: Data, part2 = false): Promise<number> {
+    let pos = 50;
+    let part1Hits = 0;
+    let part2Passes = 0;
+
+    for (const line of data) {
+        let newPos = pos;
+
+        if (line.dir == 'R') {
+            newPos = pos + line.dist;
+            part2Passes += Math.floor(newPos / 100);
+        } else {
+            newPos = pos - line.dist;
+
+            if (newPos <= 0) {
+                if (pos != 0 && newPos <= 0)
+                    part2Passes++;
+
+                part2Passes += Math.floor(Math.abs(newPos / 100));
+            }
+        }
+
+        pos = newPos % 100;
+        if (pos < 0)
+            pos += 100;
+
+        if (pos == 0)
+            part1Hits++;
+    }
+
+    if (part2)
+        return part2Passes;
+
+    return part1Hits;
 }
